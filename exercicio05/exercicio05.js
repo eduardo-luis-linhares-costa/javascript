4dd499efa54e005becbb2427358b00e3fb142da5
Eduardo Costa <a1994edu@gmail.com>
2024-05-18 14:26:56 +0100

    exercicio05 adicionado

---
+++ exercicio05/exercicio05.js
@@ -0,0 +1,28 @@
+var estudantes = [{ id: 1, nome: 'João', idade: 20 },{
+    id: 2, nome: 'Maria', idade: 22 },{ id: 3, nome:
+    'Pedro', idade: 18 },{ id: 4, nome: 'Ana', idade: 21 }];
+
+    
+
+const studentId = (id = 10) => {
+return estudantes.find( estudante => estudante.id === id )
+}
+
+console.log(studentId(2));
+
+
+
+
+const studentName = () => estudantes.map( student => `Nome do estudante: ${student.nome}`);
+
+
+console.log(studentName());
+
+
+
+const studentsAge = (age = 18) => {
+return estudantes.filter( estudante => estudante.idade > age)
+}
+
+console.log(studentsAge(20));
+
